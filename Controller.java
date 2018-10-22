ec303d496ba41064c62d08d643f08f5c44fbbb40
mnarine1 <38436299+mnarine1@users.noreply.github.com>
2018-10-22 16:13:35 -0400

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc4380;
+
+import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author aaron
+ */
+public class Controller {
+    
+    private static Model m;
+    
+    
+    public Controller()
+    {
+        m = new Model();
+    }
+    
+    
+    //This method converts the value in the left text box to the currecny specified by
+    //the right drop down.
+    public void btnConvert(View v)
+    {
+        double leftVal = convertToUSD(Double.parseDouble(v.getLeftText()), v.getLeftDropDown());
+        System.out.println(leftVal);
+        double rightVal = convertToNewCurrency(leftVal, v.getRightDropDown());
+        System.out.println(""+rightVal);
+        v.setRightVal(""+rightVal);
+    }
+    
+    
+    //Sets every text box to ""
+    public void btnClear (View v)
+    {
+        v.setLeftVal("");
+        v.setLeftDropDown("");
+        v.setRightVal("");
+        v.setRightDropDown("");
+    }
+    
+    
+    //This method takes the values of the two drop down's and swaps them with eachother
+    public void btnSwap(View v)
+    {
+        String temp = v.getRightDropDown();
+        v.setRightDropDown(v.getLeftDropDown());
+        v.setLeftDropDown(temp);
+    }
+    
+    
+    
+    
+    public void btnSignup(View v)
+    {
+        //Make a call to the database to pass in username and verify that that username does not already exist
+        //Example: m.userNameExists(String m);
+        
+        //Write the new username and password to the username database
+        //Example: m.createUser(String uName, String pWord);
+        //For this I will need a getter to get the userName and Password field for the sign up side from Michael
+        
+        //Display confirmation on screen?
+        
+        
+        
+    /*    if(!m.userNameExists(v.getLeftUserName()))
+        {
+            m.createUser(v.getLeftUserName(), v.getLeftPassword());
+        }
+        m.setCurrentUser(v.getLeftUserName());   */
+        
+        
+        m.createUser(v.getSignUpUser(), v.getSignUpPass());
+      
+    }
+    
+    public void btnLogin(View v)
+    {
+        //Made a call to the database that takes in the username and returns the correct password, so that can be compared to 
+        //the password passed in by the user
+        //Example: m.getPassword(String uName);
+        //For this I will need a getter to get the userName and Password field for the login side from Michael
+       // if(m.userNameExists(v.getRightUserName()) && m.getPassword(v.getRightUserName()).equals(v.getRightPassword()))
+        //{
+         //   m.setCurrentUser(v.getRightUserName());
+       // }
+        
+        m.login(v.getLoginUser(), v.getLoginPass());
+    }
+    
+    
+    
+    
+    private double convertToUSD(double d, String rate)
+    {
+        double xchgRate = m.getExchangeRate(rate);
+        return d*xchgRate;
+    }
+    
+    private double convertToNewCurrency(double d, String rate)
+    {
+        //The following method should return exch rate to go from USD to "rate"
+        double xchgRate = 1/m.getExchangeRate(rate);
+        return d*xchgRate;
+    }
+    
+    
+    public static void main (String[] args) {
+        View v = new View();
+        m.database();
+    }
+    
+}
---
+++ Model.java
@@ -0,0 +1,205 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc4380;
+
+import java.beans.*;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author aaron
+ */
+public class Model implements Serializable {
+    
+    private Connection con;
+    private java.sql.Statement st;
+    private ResultSet rs;
+    private String  jdbc_drivers, url, user, password = "";
+    
+    public static final String PROP_SAMPLE_PROPERTY = "sampleProperty";
+    
+    private String sampleProperty;
+    
+    private PropertyChangeSupport propertySupport;
+    
+    String currentUser;
+    
+    public Model() {
+        propertySupport = new PropertyChangeSupport(this);
+    }
+    
+    public String getSampleProperty() {
+        return sampleProperty;
+    }
+    
+    public void setSampleProperty(String value) {
+        String oldValue = sampleProperty;
+        sampleProperty = value;
+        propertySupport.firePropertyChange(PROP_SAMPLE_PROPERTY, oldValue, sampleProperty);
+    }
+    
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertySupport.addPropertyChangeListener(listener);
+    }
+    
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertySupport.removePropertyChangeListener(listener);
+    }
+
+    
+    
+    /*
+    //This method should take in a string, of the format "US Dollars", "British Pounds" , etc
+    // and should be able to return the exchange rate from USD to String rate.
+    */
+    double getExchangeRate(String rate) {
+        double x = 1;
+        try {
+            System.setProperty("jdbc.drivers", jdbc_drivers);
+ 
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/currency", "root", "");
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT * FROM exchange_rates1 WHERE Currency_name = '"+rate+"'");
+
+            if (rs.next()) {
+                System.out.println(Double.parseDouble(rs.getString(3)));
+                x = Double.parseDouble(rs.getString(3));
+            }
+
+        } catch (SQLException ex) {
+            //Logger lgr = Logger.getLogger(Version.class.getName());
+            //lgr.log(Level.SEVERE, ex.getMessage(), ex);
+               Logger.getLogger(Model.class.getName()).log(Level.SEVERE, null, ex);
+               System.out.println("Exception Caught");
+               
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+               // Logger lgr = Logger.getLogger(Version.class.getName());
+                Logger.getLogger(Model.class.getName()).log(Level.SEVERE, null, ex);
+                //lgr.log(Level.WARNING, ex.getMessage(), ex);
+                            }
+        }
+        return x;
+    }
+
+    boolean userNameExists(String uName) {
+        return true;
+    }
+
+    void createUser(String uName, String password) {
+        
+        try {
+            System.setProperty("jdbc.drivers", jdbc_drivers);
+ 
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/currency", "root", "");
+            st = con.createStatement();
+            st.executeUpdate("INSERT INTO user_info (username, password, nativeCountry, lastConversion) VALUES ('"+uName+"', '"+password+"', 'US', 'EURO')");
+
+            //if (rs.next()) {
+            //    System.out.println(rs.getString(1));
+            //}
+
+        } catch (SQLException ex) {
+            //Logger lgr = Logger.getLogger(Version.class.getName());
+            //lgr.log(Level.SEVERE, ex.getMessage(), ex);
+               Logger.getLogger(Model.class.getName()).log(Level.SEVERE, null, ex);
+               System.out.println("Exception Caught");
+               
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+               // Logger lgr = Logger.getLogger(Version.class.getName());
+                Logger.getLogger(Model.class.getName()).log(Level.SEVERE, null, ex);
+                //lgr.log(Level.WARNING, ex.getMessage(), ex);
+                            }
+        }
+    }
+    
+    void login(String uName, String password) {
+        
+        try {
+            System.setProperty("jdbc.drivers", jdbc_drivers);
+ 
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/currency", "root", "");
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT * FROM user_info WHERE username = '"+uName+"' AND password = '"+password+"'");
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            //Logger lgr = Logger.getLogger(Version.class.getName());
+            //lgr.log(Level.SEVERE, ex.getMessage(), ex);
+               Logger.getLogger(Model.class.getName()).log(Level.SEVERE, null, ex);
+               System.out.println("Exception Caught");
+               
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+               // Logger lgr = Logger.getLogger(Version.class.getName());
+                Logger.getLogger(Model.class.getName()).log(Level.SEVERE, null, ex);
+                //lgr.log(Level.WARNING, ex.getMessage(), ex);
+                            }
+        }
+    }
+
+    void setCurrentUser(String user) {
+        currentUser = user;
+    }
+
+    String getPassword(String uName) {
+        return "";
+    }
+    public void database(){
+        con = null;
+        st = null;
+        rs = null;
+        
+        jdbc_drivers = "com.mysql.jdbc.Driver";
+        url = "jdbc:mysql://localhost:3306/currency";
+        user = "root";
+        password = "";
+        
+    }
+}
